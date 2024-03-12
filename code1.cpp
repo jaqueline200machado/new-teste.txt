931fe0764d1ef7660530fceb9c7b958717406579
jaqueline200machado <113715620+jaqueline200machado@users.noreply.github.com>
2024-03-11 21:34:48 -0300

    Create code1.cpp

---
+++ code1.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main() {
+setlocale(LC_ALL, "portugeses");
+
+float mediaSalario;
+	int maiorIdade, menorIdade, qtdeHomensSalario2000 = 0;
+	char continuar, sexo;
+	
+	do{
+		printf("idade):");
+		scanf("%d", &idade);
+		
+		printf("Sexo (f/m): ");
+		scanf("%s", &sexo);
+			
+		printf("Salario: ");
+		scanf("%f", &salario);
+		
+		somaSalario += salario:
+			qtdPesquisas++;
+			
+			printf("Deseja fazer uma nova pesquisa? (s/n): ");
+			scanf("%c", &continuar);
+while(contiuar== 'S');		
+		
+		mediaSalario = somaSlario / qtdPesquisas;
+		
+		printf("Media Salarial: %.2f\n", mediaSalario);
+			printf("Maior idade:")
+	}
+} 
